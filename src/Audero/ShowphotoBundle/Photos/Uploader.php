9c973b7c14462c24656a1ebcf67458d5e8f413e1
LTRocky <rokasvaliukas@gmail.com>
2014-10-30 15:48:42 +0200

    Created service Uploader

--- src/Audero/ShowphotoBundle/Photos/Uploader.php
+++ src/Audero/ShowphotoBundle/Photos/Uploader.php
@@ -18,9 +18,39 @@ class Uploader {
         $this->client_id = $client_id;
     }
 
-    public function upload()
+
+    private function upload($post)
     {
-        echo 'veikia';
+        $timeout = 10;
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'https://api.imgur.com/3/upload');
+        curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            'Authorization: Bearer '
+        ));
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
+        $response = curl_exec($curl);
+        curl_close ($curl);
+        var_dump(json_decode($response,true));
+    }
+
+    /*
+     *  Uploads image to imgur from passed url
+     * */
+    public function uploadFromUrl($url)
+    {
+        $post   = array(
+            'image' => $url,
+            'type'  =>'url');
+
+        $this->upload($post);
+    }
+
+    public function uploadFromFile()
+    {
+
     }
 
 } 
--- src/Audero/WebBundle/Controller/MainController.php
+++ src/Audero/WebBundle/Controller/MainController.php
@@ -8,8 +8,8 @@ class MainController extends Controller
 {
     public function indexAction()
     {
-        $pm = $this->get('audero.photos.uploader');
-
+        $uploader = $this->get('audero.photos.uploader');
+        $uploader->uploadFromUrl('http://img.memerial.net/memerial.net/25/can-we-fix-it.jpg');
 
         return $this->render('AuderoWebBundle:Main:index.html.twig', array(
                 // ...
